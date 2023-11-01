12247028eceec86aed2c1fa676e1a360bb803cae
Иван Шинкаренко <ivan.shinkarenko.99@mail.ru>
2023-11-02 01:06:55 +0300

    ANIMATION+JUMP

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -7,8 +7,9 @@ public class Player : MonoBehaviour
 
     public float speed = 1f;
     public float jumpForce = 5f;
-
-
+    public bool isGrounded = false;
+    public float checkGroundOffsetY = -1.8f;
+    public float checkGroundRadius = 0.3f;
     public Animator animator;
 
     Rigidbody2D rb;
@@ -28,14 +29,39 @@ public class Player : MonoBehaviour
         float movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.05f)
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
 
         animator.SetFloat("HorizontalMove", Mathf.Abs(movement));
 
         sr.flipX = movement < 0 ? true : false;
 
+        if(isGrounded == false)
+        {
+            animator.SetBool("Jumping", true);
+        }
+        else
+        {
+            animator.SetBool("Jumping", false);
+        }
 
+        CheckGround();
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(new Vector2(transform.position.x, transform.position.y + checkGroundOffsetY), checkGroundRadius);
+
+        if(colliders.Length > 1)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 
 
